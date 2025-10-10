b725444efa21e1f43faa31bf3c8d060457149855
shivp436 <mpshivanandpatil2002@gmail.com>
2025-10-10 15:11:13 +0530

    Completed Dagster

---
+++ dbt_dagster/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dbt_dagster",
+    version="0.0.1",
+    packages=find_packages(),
+    package_data={
+        "dbt_dagster": [
+            "dbt-project/**/*",
+        ],
+    },
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "dagster-dbt",
+        "dbt-core<1.11",
+        "dbt-snowflake<1.11",
+    ],
+    extras_require={
+        "dev": [
+            "dagster-webserver",
+        ]
+    },
+)
